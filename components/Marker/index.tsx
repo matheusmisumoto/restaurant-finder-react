901d6fad01de2ea04a585bec7748d252eedd00e3
Matheus Misumoto <49180175+matheusmisumoto@users.noreply.github.com>
2025-02-17 21:48:24 -0300

    Refactor: Add Marker component with styled components and advanced marker functionality

--- components/Marker/index.tsx
+++ components/Marker/index.tsx
@@ -1,7 +1,6 @@
+import { RestaurantList } from '@/redux/interface';
 import { AdvancedMarker, InfoWindow, useAdvancedMarkerRef } from '@vis.gl/react-google-maps';
 import styled from 'styled-components';
-
-import { useDispatch } from 'react-redux';
 
 const MarkerHeader = styled.h3`
     font-family: ${(props) => props.theme.fonts.regular};
@@ -15,13 +14,19 @@ const MarkerAction = styled.p`
     font-size: .875rem;
     margin: 0 0 .5em 0;
     line-height: 1.5rem;
-    color: ${(props) => props.theme.colors.primary};
+    color: ${(props) => props.theme?.colors?.primary};
     font-weight: bold;
 `;
 
-export const Marker = ({position, restaurant, isOpen, onClick, openDetails, onClose}) => {
+export const Marker = ({position, restaurant, isOpen, onClick, openDetails, onClose} : {
+                          position: google.maps.LatLngLiteral,
+                          restaurant: RestaurantList,
+                          isOpen?: boolean,
+                          onClick?: () => void,
+                          openDetails?: () => void,
+                          onClose?: () => void
+                      }) => {
   const [markerRef, marker] = useAdvancedMarkerRef();
-  const dispatch = useDispatch();
 
   return (
     <>
