7d4bc8648f443317fd61b99247c5728adcae0b5c
Matheus Misumoto <49180175+matheusmisumoto@users.noreply.github.com>
2025-03-03 23:00:44 -0300

    Refactor: Create index file to export components for easier imports

---
+++ components/index.tsx
@@ -0,0 +1,8 @@
+export { default as Disclaimer } from './Disclaimer';
+export { default as Card } from './ImageCard';
+export { default as Loader } from './Loader';
+export { default as Map } from './Map';
+export { default as Modal } from './Modal';
+export { default as Portal } from './Modal/Portal';
+export { default as RestaurantCard } from './RestaurantCard'
+export { default as Skeleton } from './Skeleton';
