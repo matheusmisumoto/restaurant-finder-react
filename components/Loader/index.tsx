b4073b73c7b2fdb189d4f1d750420b0be99f97d7
Matheus Misumoto <49180175+matheusmisumoto@users.noreply.github.com>
2026-03-02 00:14:34 -0300

    Fix: change Lottie animation to autoPlay and update tsconfig to include .next/dev/types

--- components/Loader/index.tsx
+++ components/Loader/index.tsx
@@ -3,5 +3,5 @@ import Lottie from 'lottie-react';
 import animationData from '../../public/restaurants-loading.json';
 
 export default () => {
-    return <Lottie loop animationData={animationData} play rendererSettings={{preserveAspectRatio: 'xMidYMid slice'}} style={{margin: '2rem 0'}} />;
+    return <Lottie loop animationData={animationData} autoPlay rendererSettings={{preserveAspectRatio: 'xMidYMid slice'}} style={{margin: '2rem 0'}} />;
 }
