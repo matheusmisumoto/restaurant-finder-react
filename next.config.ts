4454724a0cdc04b921770e071dfcb494c83cc7b0
Matheus Misumoto <49180175+matheusmisumoto@users.noreply.github.com>
2026-04-11 01:44:34 -0300

    Build: update dependencies and setup Turbopack
    - Update dependencies:
      - @emotion/styled to ^11.14.1
      - @mui/icons-material and @mui/material to ^9.0.0
      - @reduxjs/toolkit to ^2.11.2
      - @vis.gl/react-google-maps to ^1.8.3
      - next to ^16.2.3
      - react and react-dom to ^19.2.5
      - styled-components to ^6.4.0
      - styled-reset to ^5.0.0
      - typescript to ^6.0.2
    - Update devDependencies:
      - @babel/core to ^7.29.0
      - @eslint/eslintrc to ^3.3.5
      - @next/eslint-plugin-next to ^16.2.3
      - @types/node to ^25.6.0
      - @types/react-dom to ^19.2.3
      - eslint-config-next to ^16.2.3
    - Change moduleResolution from "node" to "bundler" in tsconfig.json

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
 // next.config.mjs
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
+import type { NextConfig } from 'next'
+const path = require('path')
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {    
+const nextConfig: NextConfig = {
     output: 'export', // Outputs a Single-Page Application (SPA).
     distDir: './build', // Changes the build output directory to `./build`.
     turbopack: {
+        root: path.join(__dirname, '..'),
         rules: {
             '*.svg': {
                 loaders: ['@svgr/webpack'],
